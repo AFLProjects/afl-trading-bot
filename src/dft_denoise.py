3d7fae23e01dd550b2a6e295cc7c2141bb0b8a2b
AFLProjects <afluder0147@gmail.com>
2021-02-05 21:51:38 +0100

    sync dft updated

--- src/dft_denoise.py
+++ src/dft_denoise.py
@@ -2,6 +2,34 @@
 import dft_lib
 import matplotlib.pyplot as plt
 
+# Find greatest value in an array within a range
+def _max_(t, start, end):
+    _max_index_ = -1
+    _max_ = -1
+    for j in range(start, end):
+        if t[j] >= _max_:
+            _max_ = t[j]
+            _max_index_ = j
+    return [_max_,_max_index_]
+
+# Find lowest value in an array within a range
+def _min_(t, start, end):
+    _min_index_ = -1
+    _min_ = 10e12
+    for j in range(start, end):
+        if t[j] <= _min_:
+            _min_ = t[j]
+            _min_index_ = j
+    return [_min_, _min_index_]
+
+# Greatest value between two values [value,index]
+def max2(x1, x2):
+    return x1 if x1[0] >= x2[0] else x2
+
+# Lowest value between two values [value,index]
+def min2(x1, x2):
+    return x1 if x1[0] <= x2[0] else x2
+
 # Simplify graph curve, using Discrete Fourier Transform
 def denoise(graph, _threshold_):
     # Create DFT from graph
@@ -27,32 +55,18 @@ def denoise(graph, _threshold_):
                 plt.scatter(i,v,10,color="red")
                 s.append(i)
                 if(len(s) > 2):
-                    _max_ = -1
-                    _max_index_ = -1
-                    for j in range(s[len(s)-1-1], s[len(s)-1]):
-                        if graph[j] >= _max_:
-                            _max_ = graph[j]
-                            _max_index_ = j
-                    for j in range(s[len(s)-2-1], s[len(s)-1-1]):
-                        if graph[j] >= _max_:
-                            _max_ = graph[j]
-                            _max_index_ = j
-                    plt.scatter(_max_index_,_max_,10,color="blue") 
+                    r = _max_(graph, s[len(s)-1-1], s[len(s)-1])
+                    l = _max_(graph, s[len(s)-2-1], s[len(s)-1-1])
+                    k = max2(l, r)
+                    plt.scatter(k[1], k[0], 10,color="blue") 
             if v-out[i-1] > 0 and out[i+1]-v < 0:
                 plt.scatter(i,v,10,color="black")
                 s.append(i)
                 if(len(s) > 2):
-                    _min_ = 10e12
-                    _min_index_ = -1
-                    for j in range(s[len(s)-1-1], s[len(s)-1]):
-                        if graph[j] <= _min_:
-                            _min_ = graph[j]
-                            _min_index_ = j
-                    for j in range(s[len(s)-2-1], s[len(s)-1-1]):
-                        if graph[j] <= _min_:
-                            _min_ = graph[j]
-                            _min_index_ = j
-                    plt.scatter(_min_index_,_min_,10,color="blue") 
+                    r = _min_(graph, s[len(s)-1-1], s[len(s)-1])
+                    l = _min_(graph, s[len(s)-2-1], s[len(s)-1-1])
+                    k = min2(l, r)
+                    plt.scatter(k[1], k[0], 10,color="blue") 
                         
 
     return out
