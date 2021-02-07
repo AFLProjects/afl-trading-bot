2e8b9b11d784c031036038f63ee4259aa8d52231
AFLProjects <afluder0147@gmail.com>
2021-02-07 13:53:03 +0100

    update ib connection script

--- src/IBConnect.py
+++ src/IBConnect.py
@@ -8,77 +8,63 @@ import time
 
 import IBOrders
 
-
 # IB API class
 class IBapi(EWrapper, EClient):
-	# Init API
 	def __init__(self):
 		EClient.__init__(self, self)
 		self.contract_details = {} 
-	
-	# Next valide order ID
+
 	def nextValidId(self, orderId: int):
 		super().nextValidId(orderId)
 		self.nextorderId = orderId
 		print('The next valid order id is: ', self.nextorderId)
 	
-	# Output order status
 	def orderStatus(self, orderId, status, filled, remaining, avgFullPrice, permId, parentId, lastFillPrice, clientId, whyHeld, mktCapPrice):
 		print('orderStatus - orderid:', orderId, 'status:', status, 'filled', filled, 'remaining', remaining, 'lastFillPrice', lastFillPrice)
 	
-	# Output order openning
 	def openOrder(self, orderId, contract, order, orderState):
 		print('openOrder id:', orderId, contract.symbol, contract.secType, '@', contract.exchange, ':', order.action, order.orderType, order.totalQuantity, orderState.status)
 	
-	# Output order execution details
 	def execDetails(self, reqId, contract, execution):
 		print('Order Executed: ', reqId, contract.symbol, contract.secType, contract.currency, execution.execId, execution.orderId, execution.shares, execution.lastLiquidity)
 	
-	# Output contract details
 	def contractDetails(self, reqId: int, contractDetails):
 		self.contract_details[reqId] = contractDetails
 	
-	# Retrieve contract details
 	def get_contract_details(self, reqId, contract):
 		self.contract_details[reqId] = None
 		self.reqContractDetails(reqId, contract)
-		#Error checking loop - breaks from loop once contract details are obtained
+
 		for err_check in range(50):
 			if not self.contract_details[reqId]:
 				time.sleep(0.1)
 			else:
 				break
-		#Raise if error checking loop count maxed out (contract details not obtained)
+
 		if err_check == 49:
 			raise Exception('error getting contract details')
-		#Return contract details otherwise
+
 		return app.contract_details[reqId].contract
-def run_loop():
-	app.run()
 
-
-#Init app
-app = IBapi()
-app.connect('127.0.0.1', 7497, 123)
-app.nextorderId = None
-
-#Start the socket in a thread
-api_thread = threading.Thread(target=run_loop, daemon=True)
-api_thread.start()
-
-#Check if the API is connected via orderid
-while True:
-	if isinstance(app.nextorderId, int):
-		print('Connected !')
-		print()
-		break
-	else:
+def initAPI():
+	# Init API
+	app = IBapi()
+	app.connect('127.0.0.1', 7497, 123)
+	app.nextorderId = None
+	# Init thread
+	thread_exec = lambda: app.run()
+	api_thread = threading.Thread(target=thread_exec, daemon=True)
+	api_thread.start()
+	# Wait for connection
+	while not isinstance(app.nextorderId, int):
 		print('Waiting For Connection...')
 		time.sleep(1)
+	print('Connected !')
+	return app
 
-# Place order
+_API_ = initAPI()
 contract = IBOrders.IBCurrencyExchange('USD', 'EUR')
 order = IBOrders.IBMarketOrder('BUY', 10)
-app.placeOrder(app.nextorderId, contract, order)
+_API_.placeOrder(_API_.nextorderId, contract, order)
 
 
