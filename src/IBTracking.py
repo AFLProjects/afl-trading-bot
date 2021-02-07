822ac38c2aa8c62b4b524f42b0c543d4aa0f7858
AFLProjects <afluder0147@gmail.com>
2021-02-07 12:39:04 +0100

    IB Tracking

--- src/IBTracking.py
+++ src/IBTracking.py
@@ -1,13 +1,10 @@
 import urllib.request
 
-# Download Html web page
-def getHtml(url):
+# Get Price from yahoo website
+def IBGetPrice(stock):
+	html = ""
 	with urllib.request.urlopen(url) as f:
 	    html = f.read().decode('utf-8')
-	    return html
-	    
-# Get Price from yahoo website
-def getPrice(stock):
 	html = getHtml('https://finance.yahoo.com/quote/{}?p={}'.format(stock, stock))
 	html = html.split('My(6px) Pos(r) smartphone_Mt(6px)',1)[1];
 	html = html.split('<span',1)[1];
