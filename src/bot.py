ba0d85876be483ebf9ea05c4f70c83f0764b2757
AFLProjects <afluder0147@gmail.com>
2021-02-09 20:57:14 +0100

    added trending markets fetching

--- src/bot.py
+++ src/bot.py
@@ -22,18 +22,24 @@ print('Fectching trending markets...')
 opener = urllib.request.build_opener()
 opener.addheaders = [('User-Agent', 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1941.0 Safari/537.36')]
 urllib.request.install_opener(opener)
-for i in range(9):
+while True:
     time.sleep(1)
     try:
         url = f'https://finviz.com/screener.ashx?v=210&s=ta_p_tlsupport&r={index}'
         print(f'[\'url\' : \'{url}\']')
+        fecthMarkets = True
         with urllib.request.urlopen(url) as f:
             html = f.read().decode('utf-8')
             parse = (html.split('<!-- TS')[1]).split('TE -->')[0]
             for i, value in enumerate(parse.splitlines()):
                 ticker = value.split('|')[0]
-                if ticker:
+                if ticker in markets:
+                    fecthMarkets = False
+                    break
+                elif ticker:
                     markets.append(ticker)
+            if not fecthMarkets:
+                break
             index += 12
     except:
         print('No more markets')
@@ -41,9 +47,7 @@ for i in range(9):
 print(f"Found {len(markets)} markets with an uptrend.")
 print(markets)
 
-
-
 while True:
-    getCurrentPrice('AAPL')
+    time.sleep(0.1)
 
 #https://finviz.com/screener.ashx?v=210&s=ta_p_tlsupport&r=1
