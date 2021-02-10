8c618383ecbabc3250ae1a288277dfee217fae6a
AFLProjects <afluder0147@gmail.com>
2021-02-10 14:53:32 +0100

    setup.py

--- src/setup.py
+++ src/setup.py
@@ -7,13 +7,31 @@ def import_package(package):
     except ImportError:
         pip.main(['install', package])
 
+# Modules
+modules = [
+	"ibapi",
+	"matplotlib",
+	"contextlib", 
+	"yfinance", 
+	"datetime", 
+	"time", 
+	"time", 
+	"csv", 
+	"os", 
+	"sys", 
+	"urllib", 
+	"math", 
+	"requests"
+]
+
+# Check for modules
 std2.write_line('Checking for modules and installing them...')
-modules = ["ibapi", "matplotlib", "contextlib", "yfinance", "datetime", "time", "time", "csv", "os", "sys", "urllib", "math", "requests"]
 for i, module in enumerate(modules):
 	std2.write_progress_bar(i+1, len(modules), 40)
 	with std2.suppress_stdout():
 		import_package(module)
 std2.write_line('Everything is up to date !')
 
+# Exit
 pause = input('Press a key to exit.')
 
