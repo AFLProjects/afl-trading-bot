f1579020d06061440feb4031e1d8479dc3e05fd8
AFLProjects <afluder0147@gmail.com>
2021-02-17 10:44:19 +0100

    updated analysis

--- analysis/analysis.py
+++ analysis/analysis.py
@@ -1,38 +1,22 @@
 from math import *
 
-"""
-Calculates moving average
-"""
+# Calculates moving average
 def MA(graph, window_size):
-	# Calculate MA Length
-	L = len(graph) - window_size + 1
+	return [sum(graph[i : i + window_size]) / window_size for i in range(len(graph) - window_size + 1)]
 
-	# Determine MA
-	return [sum(graph[i : i + window_size]) / window_size for i in range(L)]
-
-"""
-Calculates exponential moving average
-"""
+# Calculates exponential moving average
 def EMA(graph, window_size):
-	# Init EMA
-	EMA = [50] * len(graph)
-
-	# Init first EMA with MA
+	EMA = [None] * len(graph)
 	prevEMA = sum(graph[i] for i in range(window_size)) / window_size
 	EMA[0] = prevEMA
-
-	# EMA Multiplier
 	k = (2.0 / (window_size + 1.0))
-
-	# Recursively Calculate EMA
 	for i in range(1, len(graph)):
 		EMA[i] = graph[i] * k + prevEMA * (1 - k)
 		prevEMA = EMA[i]
 	return EMA
 
-"""
-Calculates the average gain
-"""
+
+# Calculates the average gain
 def average_gain(data, offset, window_size):
 	avg = 0
 	for j in range(window_size):
@@ -43,9 +27,8 @@ def average_gain(data, offset, window_size):
 	avg /= window_size
 	return avg
 
-"""
-Calculates the average loss
-"""
+
+# Calculates the average loss
 def average_loss(data, offset, window_size):
 	avg = 0
 	for j in range(window_size):
@@ -56,9 +39,8 @@ def average_loss(data, offset, window_size):
 	avg /= window_size
 	return avg
 
-"""
-Calculates the gain
-"""
+
+# Calculates the gain
 def gain(data, offset, window_size):
 	gain = 0
 	for j in range(window_size):
@@ -68,9 +50,8 @@ def gain(data, offset, window_size):
 			gain += nxt - curr
 	return gain
 
-"""
-Calculates the loss
-"""
+
+# Calculates the loss
 def loss(data, offset, window_size):
 	loss = 0
 	for j in range(window_size):
@@ -80,42 +61,26 @@ def loss(data, offset, window_size):
 			loss += abs(nxt - curr)
 	return loss
 
-"""
-Calculate RSI
-"""
+# Calculates RSI for a single index
 def _RSI_(t):
-	# Init
 	sumGain = 0
 	sumLoss = 0
-
-	# Calculate average gain & loss
 	for i in range(1, len(t)):
 		diff = t[i] - t[i-1]
 		if diff > 0:
 			sumGain += diff
 		else:
 			sumLoss -= diff
-
-	# Process special cases
 	if sumGain == 0:
 		return 0
 	if sumLoss == 0:
 		return 100
-
-	# Calculate Relative strength
 	RS = sumGain / sumLoss
-
-	# Calculate corresponding RSI
 	return 100 - (100 / (1 + RS))
 
-"""
-Calculate RSI
-"""
+# Calculate RSI
 def RSI(graph, window_size):
-	# Init RSI
-	RSI = [50] * len(graph)
-
-	# Calculate RSI
+	RSI = [None] * len(graph)
 	for i in range(window_size, len(graph)):
 		RSI[i] = _RSI_(graph[i - window_size:i])
 	return RSI
